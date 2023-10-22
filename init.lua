68d6b8efd2ad1456e6cbb241e6c6b44159d6b889
Victor Lacorte <victor@victorlacorte.com.br>
2023-10-22 12:36:38 -0300

    Update blankline plugin

--- init.lua
+++ init.lua
@@ -103,10 +103,15 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- See `:help indent_blankline.txt`
+    -- See `:help ibl
+    main = 'ibl',
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      --indent = {
+      --  char = '┊',
+      --},
+      --whitespace = {
+      --  remove_blankline_trail = false,
+      --},
     },
   },
 
