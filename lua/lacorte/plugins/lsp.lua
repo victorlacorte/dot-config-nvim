fc983e6f7ab17d8d1166c2e112fd2ef2901f6016
Victor Lacorte <victor@victorlacorte.com.br>
2024-02-26 19:19:01 -0300

    Replace null-ls with none-ls

--- lua/lacorte/plugins/lsp.lua
+++ lua/lacorte/plugins/lsp.lua
@@ -243,7 +243,7 @@ return {
   },
 
   {
-    'jose-elias-alvarez/null-ls.nvim',
+    'nvimtools/none-ls.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = { 'mason.nvim' },
     opts = function()
