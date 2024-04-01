708e6d8c39afb77cb3e58279f41c62b85cf20095
Victor Lacorte <victor@victorlacorte.com.br>
2024-04-01 20:16:51 -0300

    Add HTML support to TS and Go config

---
+++ lua/lacorte/plugins/languages/go.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        table.insert(opts.ensure_installed, { 'go', 'gomod', 'gosum', 'gotmpl', 'gowork' })
+      end
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        gopls = {
+          -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#gopls
+        },
+      },
+    },
+  },
+}
--- lua/lacorte/plugins/languages/web.lua
+++ lua/lacorte/plugins/languages/web.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts)
       if type(opts.ensure_installed) == 'table' then
-        table.insert(opts.ensure_installed, { 'css' })
+        table.insert(opts.ensure_installed, { 'html', 'css' })
       end
     end,
   },
