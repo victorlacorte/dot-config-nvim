78ade3d60dab63eecf4707afd5a5442b2bf34d44
Victor Lacorte <1683634+victorlacorte@users.noreply.github.com>
2024-07-21 19:04:59 -0300

    Add tailwindcss to nvim-lspconfig's server table

---
+++ lua/lacorte/plugins/extras/tailwind.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        tailwindcss = {
+          -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#tailwindcss
+        },
+      },
+    },
+  },
+}
