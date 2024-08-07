98e417de45a229b921cebe02306fdef28965eb08
Victor Lacorte <1683634+victorlacorte@users.noreply.github.com>
2024-08-07 09:33:28 -0300

    Switch to Tokyo Night's night variant

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
-    opts = { style = 'moon' },
+    opts = { style = 'night' },
     config = function(_, opts)
       require('tokyonight').setup(opts)
       vim.cmd.colorscheme('tokyonight')
